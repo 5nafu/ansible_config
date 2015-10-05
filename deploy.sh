378def4bd3980b76bbba3a0493bee744433674ce
5nafu <info@tvollmer.de>
2015-10-05 21:02:07 +0200

    put Vault Password in passwords file; bootstrap new servers

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,8 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 git pull
 
-ansible-playbook site.yml --ask-vault-pass $@
+# Install predependencies Manually
+ansible all  --vault-password-file ./password.txt  -a "apt-get install python facter"
+
+# Run playbook.
+ansible-playbook site.yml --vault-password-file ./password.txt $@
