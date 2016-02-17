1bee0ac72465e28ab255a6df2c5124db414a7cbc
5nafu <info@tvollmer.de>
2016-02-17 02:45:14 +0100

    Add deployments script for workstations

---
+++ workstation.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Go to Directory of script
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+git pull
+
+echo "Installing predependencies"
+# Install predependencies Manually
+sudo apt-get install -y ansible
+
+echo "running playbook"
+# Run playbook.
+ansible-playbook workstation.yml --vault-password-file ./password.txt $@
