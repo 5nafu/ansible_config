06f20cea73abb6418b0a5d86b0d0da9d15858b6d
5nafu <info@tvollmer.de>
2016-11-23 12:00:54 +0100

    Update deploy scripts

--- router.sh
+++ router.sh
@@ -3,15 +3,13 @@
 # Go to Directory of script
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
-git pull
-
 echo "Installing predependencies on master"
 # Install predependencies Manually
 sudo apt-get install -y ansible
 
 echo "Installing predependencies on slave"
 # Install predependencies Manually
-ansible local --vault-password-file ./password.txt -m apt -a "name=facter state=present update_cache=true" -o $@
+ansible router --vault-password-file ./password.txt -m apt -a "name=facter state=present update_cache=true" -o $@
 
 echo "running playbook"
-ansible-playbook --vault-password-file ./password.txt workstation.yml $@
+ansible-playbook --vault-password-file ./password.txt router.yml $@
