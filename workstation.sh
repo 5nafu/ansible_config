55217643e47657ff081ecf14f8db3c08b58928bb
5nafu <info@tvollmer.de>
2017-03-24 16:00:00 +0100

    Add proper installation routine for origin ansible

--- workstation.sh
+++ workstation.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+install_linux() {
+    if [ -f /etc/debian_version ] ; then
+        if [ "$(lsb_release -is)" == "Ubuntu" ]; then
+            sudo apt-add-repository ppa:ansible/ansible
+        else
+            echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" | sudo tee /etc/apt/sources.list.d/ansible-ubuntu-ansible-xenial.list
+            sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+        fi
+        sudo apt-get update
+        sudo apt-get install -y ansible
+    elif [ -f /etc/redhat-release ] ; then
+        sudo yum install ansible
+    else
+      echo "Unsupported Linux Distro!" >&2
+      return 2
+    fi
+}
+
+
 # Go to Directory of script
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
@@ -7,8 +26,12 @@ git pull
 
 command -v ansible >/dev/null 2>&1 || {
   echo "Installing predependencies on master"
-  # Install predependencies Manually
-  sudo apt-get install -y ansible
+  if [ "${uname -s}" == "Linux" ]; then
+    install_linux || exit
+  else
+    echo "Unsupported Plattform. Please install ansible manually." >&2
+    exit
+  fi
 }
 
 echo "Installing predependencies on slave"
